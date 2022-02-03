5e8c11f2c6467c8f912b7c9609532b97d1da16b3
Zen Dejavu <arsanptik05@gmail.com>
2022-02-03 16:53:27 +0800

    public files and views/portfolio

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,123 @@
+<div class="section-inner custom-page-content">
+    <div class="section-title-block second-style">
+        <h2 class="section-title">Portfolio</h2>
+        <h5 class="section-description">Our Works</h5>
+    </div>
+
+    <div class="section-content">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12">
+                <!-- Portfolio Content -->
+                <div class="portfolio-content">
+
+                    {{-- <div class="card bg-dark" style="width: 18rem;">
+                        <img src="images/portfolio/4.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h3 class="card-title">Smart Reader SPC</h3>
+                          <span>IoT</span>
+                              <span style="display: flex; text-align: left; flex-direction: column;">Client</span>
+                              <span style="display: flex; text-align: right; flex-direction: column;">Year</span>
+
+                          <span style="display: flex; text-align: left; flex-direction: column;">Bapenda Sulsel</span>
+                          <span style="display: flex; text-align: right; flex-direction: column;">2020</span>
+                        </div>
+                      </div> --}}
+
+                    
+                    <ul class="portfolio-filters">
+                        <li class="active">
+                            <a class="filter btn btn-sm btn-link" data-group="category_all">All</a>
+                        </li>
+                        <li>
+                            <a class="filter btn btn-sm btn-link" data-group="category_media">Media</a>
+                        </li>
+                        <li>
+                            <a class="filter btn btn-sm btn-link" data-group="category_mockups">Mockups</a>
+                        </li>
+                        <li>
+                            <a class="filter btn btn-sm btn-link" data-group="category_soundcloud">SoundCloud</a>
+                        </li>
+                        <li>
+                            <a class="filter btn btn-sm btn-link" data-group="category_vimeo-videos">Vimeo Videos</a>
+                        </li>
+                        <li>
+                            <a class="filter btn btn-sm btn-link" data-group="category_youtube-videos">YouTube Videos</a>
+                        </li>
+                    </ul>
+
+                    <!-- Portfolio Grid -->
+                    <div class="portfolio-grid three-columns">
+
+                        <figure class="item lbaudio" data-groups='["category_all", "category_soundcloud"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/1.jpg" alt="SoundCloud Audio" title="" />
+                                <a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/221650664&#038;color=%23ff5500&#038;auto_play=false&#038;hide_related=false&#038;show_comments=true&#038;show_user=true&#038;show_reposts=false&#038;show_teaser=true&#038;visual=true"
+                                    class="lightbox mfp-iframe" title="SoundCloud Audio"></a>
+                            </div>
+
+                            <i class="fa fa-volume-up"></i>
+                            <h4 class="name">SoundCloud Audio</h4>
+                            <span class="category">SoundCloud</span>
+                        </figure>
+
+                        <figure class="item standard" data-groups='["category_all", "category_media"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/2.jpg" alt="Media Project 2" title="" />
+                                <a href="portfolio-1.html" class="ajax-page-load"></a>
+                            </div>
+
+                            <i class="far fa-file-alt"></i>
+                            <h4 class="name">Media Project 2</h4>
+                            <span class="category">Media</span>
+                        </figure>
+
+                        <figure class="item lbvideo" data-groups='["category_all", "category_vimeo-videos"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/3.jpg" alt="Vimeo Video 1" title="" />
+                                <a href="https://player.vimeo.com/video/158284739" class="lightbox mfp-iframe" title="Vimeo Video 1"></a>
+                            </div>
+
+                            <i class="fas fa-video"></i>
+                            <h4 class="name">Vimeo Video 1</h4>
+                            <span class="category">Vimeo Videos</span>
+                        </figure>
+
+                        <figure class="item standard" data-groups='["category_all", "category_media"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/4.jpg" alt="Media Project 1" title="" />
+                                <a href="portfolio-1.html" class="ajax-page-load"></a>
+                            </div>
+
+                            <i class="far fa-file-alt"></i>
+                            <h4 class="name">Media Project 1</h4>
+                            <span class="category">Media</span>
+                        </figure>
+
+                        <figure class="item lbimage" data-groups='["category_all", "category_mockups"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/5.jpg" alt="Mockup Design 1" title="" />
+                                <a class="lightbox" title="Mockup Design 1" href="images/portfolio/5.jpg"></a>
+                            </div>
+
+                            <i class="far fa-image"></i>
+                            <h4 class="name">Mockup Design 1</h4>
+                            <span class="category">Mockups</span>
+                        </figure>
+
+                        <figure class="item lbvideo" data-groups='["category_all", "category_youtube-videos"]'>
+                            <div class="portfolio-item-img">
+                                <img src="images/portfolio/6.jpg" alt="YouTube Video 1" title="" />
+                                <a href="https://www.youtube.com/watch?v=BYRJMEkoWCI" class="lightbox mfp-iframe" title="YouTube Video IEEE HAC Training"></a>
+                            </div>
+
+                            <i class="fas fa-video"></i>
+                            <h4 class="name">YouTube Video 1</h4>
+                            <span class="category">YouTube Videos</span>
+                        </figure>
+                    </div>
+                </div>
+                <!-- /Portfolio Content -->
+            </div>
+        </div>
+    </div>
+</div>
